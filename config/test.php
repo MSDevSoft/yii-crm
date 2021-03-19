cee68f3ce48c3700aca8fd8a151e560cf7546dd8
Mateusz Win10 <mszumiela@gmail.com>
2021-03-19 19:55:38 +0100

    improve configs files

--- config/test.php
+++ config/test.php
@@ -1,6 +1,6 @@
 <?php
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/test_db.php';
+$params = require __DIR__ . '/params.local.php';
+$db = require __DIR__ . '/test_db.local.php';
 
 /**
  * Application configuration shared by all test types
